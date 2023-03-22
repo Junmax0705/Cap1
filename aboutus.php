7fe182d7c8bd2aa13855e427a1109e594da77905
Junmax0705 <119088182+Junmax0705@users.noreply.github.com>
2023-03-22 11:57:58 +0800

    commit
    commit

---
+++ aboutus.php
@@ -0,0 +1,198 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+
+    <link
+  rel="stylesheet"
+  href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
+/>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<style>
+    *{
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+	font-family: 'Quicksand', sans-serif;
+}
+
+body{
+	height: 100vh;
+	width: 100%;
+}
+
+.container{
+	position: relative;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 20px 100px;
+}
+
+.container:after{
+	content: '';
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	left: 0;
+	top: 0;
+	background: url("img/bg.jpg") no-repeat center;
+	background-size: cover;
+	filter: blur(50px);
+	z-index: -1;
+}
+.contact-box{
+	max-width: 850px;
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	background-color: #fff;
+	box-shadow: 0px 0px 19px 5px rgba(0,0,0,0.19);
+    border-radius: 20px;
+}
+
+.left{
+	background: url("img/img3.jpg") no-repeat center;
+	background-size: cover;
+	height: 100%;
+    border-radius: 20px;
+}
+
+.right{
+	padding: 25px 40px;
+}
+
+h2{
+	position: relative;
+	padding: 0 0 10px;
+	margin-bottom: 10px;
+    font-size: 3rem;
+}
+
+h2:after{
+	content: '';
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    transform: translateX(-50%);
+    height: 4px;
+    width: 50px;
+    border-radius: 2px;
+    background-color: black;
+}
+
+.field{
+	width: 100%;
+	border: 2px solid rgba(0, 0, 0, 0);
+	outline: none;
+	background-color: rgba(230, 230, 230, 0.6);
+	padding: 0.5rem 1rem;
+	font-size: 1.8rem;
+	margin-bottom: 22px;
+	transition: .3s;
+}
+
+.field:hover{
+	background-color: rgba(0, 0, 0, 0.1);
+}
+
+textarea{
+	min-height: 150px;
+}
+
+.btn{
+	width: 100%;
+	padding: 0.5rem 1rem;
+	background-color:red;
+	color: #fff;
+	font-size: 2rem;
+	border: none;
+	outline: none;
+	cursor: pointer;
+	transition: .3s;
+}
+
+.btn:hover{
+    background-color: #27ae60;
+}
+
+.field:focus{
+    border: 2px solid rgba(30,85,250,0.47);
+    background-color: #fff;
+}
+
+@media screen and (max-width: 880px){
+	.contact-box{
+		grid-template-columns: 1fr;
+	}
+	.left{
+		height: 200px;
+	}
+}
+</style>
+<header>
+    <a href="#" class="logo"><i class="fa-brands fa-golang"></i>Anime.</a>
+    <nav class="navbar">
+         <a href="index.php">home</a>
+         <a href="#anime">List</a>
+         <a href="#child">Updates</a>
+         <a href="#family">About Us</a>
+    </nav>
+    <div class="icons">
+        <i class="fas fa-bars" id="menu-bars"></i>
+        <i class="fas fa-search" id="search-icon"></i>
+        <a href="aboutus.html" class="fas fa-heart"></a>
+        <a href="signup.html" class="fas fa-user"></a>
+    </div>
+</header>
+
+
+
+
+
+<body>
+
+    <form
+                action="https://formbold.com/s/6Qrdp"
+                method="POST"
+              >
+	<div class="container">
+		<div class="contact-box">
+			<div class="left"></div>
+			<div class="right">
+				<h2>Contact Us</h2>
+
+				<input type="text" class="field" placeholder="Your Name">
+				<input type="text" class="field" placeholder="Your Email">
+				<input type="text" class="field" placeholder="Phone">
+				<textarea placeholder="Message" class="field"></textarea>
+				<button class="btn">Send</button>
+			</div>
+		</div>
+	</div>
+    <script src="main.js"></script>
+</form>
+</body>
+</html>
---
+++ kakegurui.php
@@ -0,0 +1,113 @@
+<?php
+
+
+
+?>
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demon Slayer</title>
+
+    <link
+  rel="stylesheet"
+  href="https://unpkg.com/swiper@7/swiper-bundle.min.css"
+/>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    
+    <!-- header section start -->
+    <header>
+        <a href="index.php" class="logo"><i class="fa-brands fa-golang"></i>Anime.</a>
+        <nav class="navbar">
+             <a href="index.php">home</a>
+             <a href="#">List</a>
+             <a href="#">Updates</a>
+             <a href="#">About Us</a>
+        </nav>
+        <div class="icons">
+            <i class="fas fa-bars" id="menu-bars"></i>
+            <i class="fas fa-search" id="search-icon"></i>
+            <a href="aboutus.php" class="fas fa-heart"></a>
+            <a href="signup.php" class="fas fa-user"></a>
+        </div>
+    </header>
+    
+    <div class="container">
+        <img src="img/img7.jpeg" style="max-width: 100%;height: auto;">
+    </div>
+    <h1 class="heading">Kakegurui:Compulsive Gambler</h1>
+    <h2 style="font-size: 1.5rem;color:#fff;text-decoration: none;text-align: justify;padding: 5rem;line-height: 2;"> "Kimetsu no Yaiba," also known as "Demon Slayer," is a popular manga and anime series created by Koyoharu Gotouge. The story takes place in a fictionalized version of Japan during the Taisho era, where demons roam the land and threaten human existence.
+
+        The main protagonist of the series is a young boy named Tanjiro Kamado, whose family is killed by demons, except for his sister Nezuko, who is turned into a demon herself.</h2>
+
+    <video width="100%" height="auto" controls>
+        <source src="video/kakegurui.mp4">
+    </video>
+    <br>
+    <h1 class="heading">List of Episodes</h1>
+
+    <div class="container">
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 1</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch DKakegurui:Compulsive GamblerS1 episode 2</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 3</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 4</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 5</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 6</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 7</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 8</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 9</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 10</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 11</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 12</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 13</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 14</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 15</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 16</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 17</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 18</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 19</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 20</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 21</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 22</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 23</a>
+        <a href="#" class="btn" style="width: 38%;height: auto; padding: 20px;margin-top: 20px; margin-bottom: 20px; margin-left: 5%;margin-right: 5%;">Watch Kakegurui:Compulsive Gambler S1 episode 24</a>
+    </div>
+    
+    <!------Footer------>
+    <div class="copyright container">
+        <a href="#" class="logo"><i class="fa-brands fa-golang"></i>Anime.</a>
+        <p>&#169; Junmaxie. All rights reserved.</p>
+        <div class="socail-links mt-3" style="font-size: xx-large;color: red;">
+            <a href="https://twitter.com/" target="_blank" class="twiiter"><i class="fa-brands fa-twitter"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100089673155790" target="_blank" alt="facebook" class="twiiter"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="https://myaccount.google.com/" target="_blank" class="twiiter"><i class="fa-brands fa-google-plus"></i></a>
+            <a href="https://www.instagram.com/" target="_blank" class="twiiter"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://www.linkedin.com/login" target="_blank" class="twiiter"><i class="fa-brands fa-linkedin-in"></i></a>
+        </div>
+    </div>    
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+    <!-- custome js file link -->
+    <script src="main.js"></script>
+</body>
+</html>
